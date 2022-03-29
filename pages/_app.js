6cda4ab01368944945c94208e6ab03fd34c1b576
Frank Xin <91763541+xinfrank@users.noreply.github.com>
2022-03-29 17:16:57 -0500

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="afterInteractive" id="google-analytics-script">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', ${process.env.NEXT_GOOGLE_ANALYTICS});`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
